e4b7ce0ad143102c21ada4acc1fdedc7a00175bb
Triple-V8 <3plev.eth@Victors-MacBook-Pro.local>
2023-12-31 17:38:44 +0100

    Yet to update the various game modes

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client"; 
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import Header from './Components/Header';
+import Header from './Components/header';
 import Categories from './Components/categories';
 import Features from './Components/features';
 import Toolspartners from './Components/tools_partners';
