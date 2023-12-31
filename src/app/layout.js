1164494f9fd3faf0d7c46dbdc39ea5809f9d834e
Triple-V8 <3plev.eth@Victors-MacBook-Pro.local>
2023-12-31 21:53:29 +0100

    updated v3 logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,19 +13,19 @@ export default function RootLayout({ children }) {
     
       <html lang="en">
          <meta
-      property="description"
-      content="Blockchain-powered lottery, unleashing the future of gaming!"
-    />
-    <title>Delots</title>
-    <meta property="og:title" content="Delots" />
-     <meta
-      property="og:image"
-      content="./Components/vlogo.png"
-    />
-    <meta name="twitter:card" content="./Components/vlogo.png" />
-  <meta name="twitter:title" content="Delots" />
-  <meta name="twitter:description" content="Blockchain-powered lottery, unleashing the future of gaming!" />
-  <meta name="twitter:image" content="./Components/vlogo.png" />
+          property="description"
+          content="Blockchain-powered lottery, unleashing the future of lotteries!"
+        />
+        <title>Delots</title>
+        <meta property="og:title" content="Delots" />
+        <meta
+          property="og:image"
+          content="./Components/vlogo.png"
+        />
+        <meta name="twitter:card" content="./Components/vlogo.png" />
+        <meta name="twitter:title" content="Delots" />
+        <meta name="twitter:description" content="Blockchain-powered lottery, unleashing the future of lotteries!" />
+        <meta name="twitter:image" content="./Components/vlogo.png" />
         <body className={inter.className}>{children}</body>
       </html>
     
