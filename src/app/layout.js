c87fe9d6e0a0f5c5d1b37bbf3b39fd5ff7dfa763
Triple-V8 <3plev.eth@Victors-MacBook-Pro.local>
2023-12-31 21:41:49 +0100

    updated v3 logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
+<link rel="icon" href="./favicon.ico" sizes="any" />
+
 const inter = Inter({ subsets: ['latin'] })
 
 
@@ -10,6 +12,10 @@ export default function RootLayout({ children }) {
   return (
     
       <html lang="en">
+         <meta
+      property="description"
+      content="Blockchain-powered lottery, unleashing the future of gaming!"
+    />
         <body className={inter.className}>{children}</body>
       </html>
     
